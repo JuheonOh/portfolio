5fd1920828b75efeea3eb92714e700f51d116d8f
Randolph <dhwngjs01@naver.com>
2025-11-19 17:33:17 +0900

    feat: 소스코드 링크 추가, 수상 내역 순서 변경

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -5,35 +5,118 @@ import scrollSection from "../utils/scrollSection";
 export default function Awards() {
   const infoArr = [
     {
-      imageSrc: "2016_national.jpg",
-      imageAlt: "2016 서울특별시 제51회 전국기능경기대회 동메달 수상",
-      infoTitle: <h5>2016 서울특별시 제51회 전국기능경기대회 동메달 수상</h5>,
-    },
-    {
       imageSrc: "2017_national.jpg",
       imageAlt: "2017 제주특별자치도 제52회 전국기능경기대회 우수상(6등) 수상",
-      infoTitle: <h5>2017 제주특별자치도 제52회 전국기능경기대회 우수상&#40;6등&#41; 수상</h5>,
+      infoTitle: (
+        <h5>
+          2017 제주특별자치도 제52회 전국기능경기대회 우수상&#40;6등&#41;
+          수상&nbsp;
+          <span>
+            &#40;
+            <a
+              href="https://github.com/JuheonOh/webskills/tree/2017_%EC%A0%84%EA%B5%AD"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 underline"
+            >
+              Github
+            </a>
+            &#41;
+          </span>
+        </h5>
+      ),
     },
+    {
+      imageSrc: "2016_national.jpg",
+      imageAlt: "2016 서울특별시 제51회 전국기능경기대회 동메달 수상",
+      infoTitle: (
+        <h5>
+          2016 서울특별시 제51회 전국기능경기대회 동메달 수상&nbsp;
+          <span>
+            &#40;
+            <a
+              href="https://github.com/JuheonOh/webskills/tree/2016_%EC%A0%84%EA%B5%AD"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 underline"
+            >
+              Github
+            </a>
+            &#41;
+          </span>
+        </h5>
+      ),
+    },
+
     {
       imageSrc: "worldskills_korea.jpg",
       imageAlt: "국제기능올림픽대회 한국위원회",
       infoTitle: (
         <>
-          <h5>2017 광주광역시 지방기능경기대회 금메달 수상</h5>
-          <h5>2016 광주광역시 지방기능경기대회 금메달 수상</h5>
-          <h5>2015 광주광역시 지방기능경기대회 동메달 수상</h5>
+          <h5>
+            2017 광주광역시 지방기능경기대회 금메달 수상&nbsp;
+            <span>
+              &#40;
+              <a
+                href="https://github.com/JuheonOh/webskills/tree/2017_%EC%A7%80%EB%B0%A9"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline"
+              >
+                Github
+              </a>
+              &#41;
+            </span>
+          </h5>
+          <h5>
+            2016 광주광역시 지방기능경기대회 금메달 수상&nbsp;
+            <span>
+              &#40;
+              <a
+                href="https://github.com/JuheonOh/webskills/tree/2016_%EC%A7%80%EB%B0%A9"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline"
+              >
+                Github
+              </a>
+              &#41;
+            </span>
+          </h5>
+          <h5>
+            2015 광주광역시 지방기능경기대회 동메달 수상&nbsp;
+            <span>
+              &#40;
+              <a
+                href="https://github.com/JuheonOh/webskills/tree/2015_%EC%A7%80%EB%B0%A9"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline"
+              >
+                Github
+              </a>
+              &#41;
+            </span>
+          </h5>
         </>
       ),
     },
   ];
 
   return (
-    <section id="awards" className="relative w-full min-h-screen py-28">
-      <div className="absolute inset-0 bg-cover bg-no-repeat bg-top isolate -z-10" style={{ backgroundImage: "url(images/career/career_bg.jpg)" }}></div>
-      <div className="absolute inset-0 bg-black opacity-50 -z-10"></div>
+    <section id="awards" className="relative min-h-screen w-full py-28">
+      <div
+        className="absolute inset-0 isolate -z-10 bg-cover bg-top bg-no-repeat"
+        style={{ backgroundImage: "url(images/career/career_bg.jpg)" }}
+      ></div>
+      <div className="absolute inset-0 -z-10 bg-black opacity-50"></div>
       <div className="container mx-auto px-6">
-        <h2 className="flex justify-center items-center text-center text-5xl font-black gap-4 text-white mb-12">
-          <FontAwesomeIcon icon={faLink} className="text-3xl cursor-pointer" onClick={() => scrollSection("awards")} />
+        <h2 className="mb-12 flex items-center justify-center gap-4 text-center text-5xl font-black text-white">
+          <FontAwesomeIcon
+            icon={faLink}
+            className="cursor-pointer text-3xl"
+            onClick={() => scrollSection("awards")}
+          />
           <span className="border-b-2 border-gray-200 pb-1">AWARDS</span>
         </h2>
 
@@ -49,10 +132,14 @@ export default function Awards() {
 
 function CareerComponent({ imageSrc, imageAlt, infoTitle, infoDesc }) {
   return (
-    <div className="flex items-center gap-x-12 p-10 rounded-2xl bg-white/90 shadow-lg shadow-gray-500/50">
+    <div className="flex items-center gap-x-12 rounded-2xl bg-white/90 p-10 shadow-lg shadow-gray-500/50">
       <div className="w-1/4">
-        <div className="w-full aspect-square rounded-full overflow-hidden border border-gray-200">
-          <img src={`images/career/${imageSrc}`} alt={imageAlt} className="w-full h-full object-cover" />
+        <div className="aspect-square w-full overflow-hidden rounded-full border border-gray-200">
+          <img
+            src={`images/career/${imageSrc}`}
+            alt={imageAlt}
+            className="h-full w-full object-cover"
+          />
         </div>
       </div>
       <div className="w-3/4">
